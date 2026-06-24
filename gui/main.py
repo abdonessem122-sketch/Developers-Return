8cc9b34d527a446da6c66989b6cf69d7f66e46e7
abdonessem122-sketch <abdonessem122@gmail.com>
2026-06-24 17:58:24 +0000

    Updated README and added new exploits

--- gui/main.py
+++ gui/main.py
@@ -11,3 +11,19 @@ else:
 
 root = Window(800 , 600 , "Developer Return 1.0 Alpha")
 root.create_menu_bar()
+
+class empty:
+    def __init__(self):
+        pass
+
+data = exploit.fu_read(empty)
+
+print (f"Data is {data}")
+
+exploit.fu_write (data , 0xDEADBEEF)
+
+ndata = exploit.fu_read(data)
+
+print (f"After write : {ndata}")
+
+exploit.fu_write(data , data)
